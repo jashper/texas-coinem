41edfe7017aad19af9f4e39d378942502b41d3f7
jashper <tommy_a@mit.edu>
2013-07-24 11:49:09 -0400

    newHand() refactor complete

--- Server/Blinds.go
+++ Server/Blinds.go
@@ -6,22 +6,14 @@ type BlindLevel struct {
 }
 
 type Blinds struct {
-	blinds []BlindLevel
+	levels []BlindLevel
 }
 
 func (this *Blinds) Init(sbs, antes []float64) {
 	levels := len(sbs)
-	this.blinds = make([]BlindLevel, levels)
+	this.levels = make([]BlindLevel, levels)
 	for i := 0; i < levels; i++ {
-		this.blinds[i].sb = sbs[i]
-		this.blinds[i].ante = antes[i]
+		this.levels[i].sb = sbs[i]
+		this.levels[i].ante = antes[i]
 	}
-}
-
-func (this Blinds) GetSB(level int) float64 {
-	return this.blinds[level].sb
-}
-
-func (this Blinds) GetAnte(level int) float64 {
-	return this.blinds[level].ante
 }
--- Server/GameInstanceB.go
+++ Server/GameInstanceB.go
@@ -1,6 +1,7 @@
 package Server
 
 import (
+	"math"
 	"sync/atomic"
 )
 
@@ -14,6 +15,7 @@ type PlayerInfo struct {
 	stack       float64
 	pot         float64
 	hand        []int
+	hasFolded   bool
 }
 
 type GameInstance struct {
@@ -30,6 +32,7 @@ type GameInstance struct {
 	currentPlayer   int
 	currentLA       LegalActions
 	streetEndPlayer int
+	activeCount     int
 
 	deck       [52]int
 	deckIdx    int
@@ -122,7 +125,74 @@ func (g *GameInstance) newTurn(playerID int32) {
 }
 
 func (g *GameInstance) newHand() {
+	select {
+	case interrupt := <-g.interrupts:
+		g.handleInterrupt(interrupt)
+	default:
+		break
+	}
 
+	g.deck = <-g.context.Entropy.Decks
+	g.deckIdx = 0
+	g.handID++
+	g.boardCards = g.boardCards[0:0]
+
+	activeCount = 0
+	for i := 0; i < len(g.players); i++ {
+		p := g.players[i]
+		p.hand = p.hand[0:0]
+		p.pot = 0
+		p.hasFolded = false
+
+		if p.stack > 0 {
+			activeCount++
+		}
+	}
+
+	if activeCount == 1 {
+		//TODO: Signal end of game
+	}
+
+	level := g.parameters.Blinds.levels[g.blindLevel]
+	g.sb = level.sb
+	g.bb = g.sb * 2
+	g.ante = level.ante
+
+	g.buttonPlayer == g.getNextPlayer(g.buttonPlayer)
+
+	sbIdx := g.getNextPlayer(g.buttonPlayer)
+	sbPlayer := g.players[sbIdx]
+	if g.sb > sbPlayer.stack {
+		sbPlayer.pot = sbPlayer.stack
+	} else {
+		sbPlayer.pot = g.sb
+	}
+
+	bbIdx := g.getNextPlayer(sbIdx)
+	bbPlayer := g.players[bbIdx]
+	if g.bb > bbPlayer.stack {
+		bbPlayer.pot = bbPlayer.stack
+	} else {
+		bbPlayer.pot = g.bb
+	}
+
+	if g.ante > 0 {
+		for i := 0; i < len(g.players); i++ {
+			chips := g.getChipsAvailable(i)
+			if g.ante > chips {
+				g.players[i].pot += chips
+			} else {
+				g.players[i].pot += g.ante
+			}
+		}
+	}
+
+	g.streetEndPlayer = -1
+	g.maxPot = g.bb + g.ante
+	g.minBet = g.bb
+
+	g.logic.DealCards(g)
+	g.newTurn(g.getNextPlayer(bbIdx))
 }
 
 // ######################
@@ -223,4 +293,24 @@ func (g *GameInstance) getChipsAvailable(playerID int) float64 {
 
 func (g *GameInstance) getChipsUnpaid(playerID int) float64 {
 	return (g.maxPot - g.players[playerID].pot)
+}
+
+func (g *GameInstance) handleInterrupt(interrupt GameInterrupt) {
+	if interrupt.iType == I_GAME_NEW_BLINDS {
+		g.blindLevel++
+		go QueueBlindsTimer(g.parameters.LevelTime, g)
+	}
+}
+
+func (g *GameInstance) getNextPlayer(current int) int {
+	for g.players[current].pot == g.players[current].stack ||
+		g.players[current].hasFolded {
+		if current < len(g.players)-1 {
+			current++
+		} else {
+			current = 0
+		}
+	}
+
+	return current
 }
